045f96302eb5f59740b8280af7d4bdaa20cc5733
Sam Bardsley <93604147+SamBardsley@users.noreply.github.com>
2022-06-14 17:31:42 +0000

    Baked in old html

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,50 @@
+const btn         = document.getElementById('toggle_btn');
+
+btn.addEventListener('click', function handleClick() {
+  if (document.body.className == "dark") {
+    document.body.className = "light";
+    btn.querySelector('ion-icon').setAttribute('name', 'moon-outline');
+  } else {
+    document.body.className = "dark";
+    btn.querySelector('ion-icon').setAttribute('name', 'sunny-outline');
+  }
+});
+
+function jump001(field, autoMove) {
+  if(field.value.length >= field.maxLength) {
+    console.log(this);
+    field.nextElementSibling.focus();
+    //document.getElementById("").focus();
+  }
+}
+
+// rest of code
+var myInputs = document.querySelectorAll(".grid");
+var wordleWord = "smart";
+var myGuess = "";
+var letterCount = 0;
+
+console.log(myInputs);
+
+for (var i=0; i<myInputs.length; i++) {
+  myInputs[i].addEventListener("change", checkLetter);
+}
+
+function checkLetter(e) {
+  letterCount++;
+  if(letterCount == 5) console.log();
+  else console.log();
+
+  // check to see if all input boxes in row have a letter value
+  // we will need to get each input box in the row
+  // loop over, see if they are all filled, get word
+
+  // splitting up the guess into individual letters, answer too
+  // each guess letter against each answer letter 
+  // will be two for loops
+}
+
+
 var stompClient = null;
 
 function setConnected(connected) {
@@ -63,49 +110,3 @@ $(function () {
     $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
 });
-
-const btn         = document.getElementById('toggle_btn');
-
-btn.addEventListener('click', function handleClick() {
-  if (document.body.className == "dark") {
-    document.body.className = "light";
-    btn.querySelector('ion-icon').setAttribute('name', 'moon-outline');
-  } else {
-    document.body.className = "dark";
-    btn.querySelector('ion-icon').setAttribute('name', 'sunny-outline');
-  }
-});
-
-function jump001(field, autoMove) {
-  if(field.value.length >= field.maxLength) {
-    console.log(this);
-    field.nextElementSibling.focus();
-    //document.getElementById("").focus();
-  }
-}
-
-// rest of code
-var myInputs = document.querySelectorAll(".grid");
-var wordleWord = "smart";
-var myGuess = "";
-var letterCount = 0;
-
-console.log(myInputs);
-
-for (var i=0; i<myInputs.length; i++) {
-  myInputs[i].addEventListener("change", checkLetter);
-}
-
-function checkLetter(e) {
-  letterCount++;
-  if(letterCount == 5) console.log();
-  else console.log();
-
-  // check to see if all input boxes in row have a letter value
-  // we will need to get each input box in the row
-  // loop over, see if they are all filled, get word
-
-  // splitting up the guess into individual letters, answer too
-  // each guess letter against each answer letter 
-  // will be two for loops
-}
